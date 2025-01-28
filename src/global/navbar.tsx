122c85daf13c3f8b09c79ca5db6bec7e819d0bbf
tejasvi@appeneure <tejasvi@appeneure.com>
2025-01-28 22:32:10 +0530

    added

--- src/global/navbar.tsx
+++ src/global/navbar.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Navbar = async (Props: Props) => {
   return (
-    <div>navbar</div>
-  )
-}
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between ">
+      
+      navbar
+    </header>
+  );
+};
 
 export default Navbar;
