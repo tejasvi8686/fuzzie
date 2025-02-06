63b4ddaf238c5a9ddac5ed022fb6889debf99bc4
tejasvi@appeneure <tejasvi@appeneure.com>
2025-02-06 23:21:04 +0530

    added

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,11 +1,17 @@
-import Category from '@/components/icons/category'
-import Logs from '@/components/icons/clipboard'
-import Templates from '@/components/icons/cloud_download'
-import Home from '@/components/icons/home' 
-import Payment from '@/components/icons/payment'
-import Settings from '@/components/icons/settings'
-import Workflows from '@/components/icons/workflows'
-import { Connection } from './types'
+// import Category from '@/components/icons/category'
+// import Logs from '@/components/icons/clipboard'
+// import Templates from '@/components/icons/cloud_download'
+// import Home from '@/components/icons/home' 
+// import Payment from '@/components/icons/payment'
+// import Settings from '@/components/icons/settings'
+// import Workflows from '@/components/icons/workflows'
+// import { Connection } from './types'
+
+import Category from "@/components/icons/category"
+import Templates from "@/components/icons/cloud_download"
+import Payment from "@/components/icons/payment"
+import Workflows from "@/components/icons/workflows"
+import { Home, Logs, Settings } from "lucide-react"
 
 export const clients = [...new Array(10)].map((client, index) => ({
   href: `/${index + 1}.png`,
@@ -147,35 +153,35 @@ export const EditorCanvasDefaultCardTypes = {
   },
 }
 
-export const CONNECTIONS: Connection[] = [
-  {
-    title: 'Google Drive',
-    description: 'Connect your google drive to listen to folder changes',
-    image: '/googleDrive.png',
-    connectionKey: 'googleNode',
-    alwaysTrue: true,
-  },
-  {
-    title: 'Discord',
-    description: 'Connect your discord to send notification and messages',
-    image: '/discord.png',
-    connectionKey: 'discordNode',
-    accessTokenKey: 'webhookURL',
-  },
-  {
-    title: 'Notion',
-    description: 'Create entries in your notion dashboard and automate tasks.',
-    image: '/notion.png',
-    connectionKey: 'notionNode',
-    accessTokenKey: 'accessToken',
-  },
-  {
-    title: 'Slack',
-    description:
-      'Use slack to send notifications to team members through your own custom bot.',
-    image: '/slack.png',
-    connectionKey: 'slackNode',
-    accessTokenKey: 'slackAccessToken',
-    slackSpecial: true,
-  },
-]
+// export const CONNECTIONS: Connection[] = [
+//   {
+//     title: 'Google Drive',
+//     description: 'Connect your google drive to listen to folder changes',
+//     image: '/googleDrive.png',
+//     connectionKey: 'googleNode',
+//     alwaysTrue: true,
+//   },
+//   {
+//     title: 'Discord',
+//     description: 'Connect your discord to send notification and messages',
+//     image: '/discord.png',
+//     connectionKey: 'discordNode',
+//     accessTokenKey: 'webhookURL',
+//   },
+//   {
+//     title: 'Notion',
+//     description: 'Create entries in your notion dashboard and automate tasks.',
+//     image: '/notion.png',
+//     connectionKey: 'notionNode',
+//     accessTokenKey: 'accessToken',
+//   },
+//   {
+//     title: 'Slack',
+//     description:
+//       'Use slack to send notifications to team members through your own custom bot.',
+//     image: '/slack.png',
+//     connectionKey: 'slackNode',
+//     accessTokenKey: 'slackAccessToken',
+//     slackSpecial: true,
+//   },
+// ]
