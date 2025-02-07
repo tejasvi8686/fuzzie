80b2af3a03524d827b86a90cd87d90323c59a4d1
tejasvi@appeneure <tejasvi@appeneure.com>
2025-02-07 23:33:44 +0530

    navbar and dashboard and side abr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
                     Start For Free Today
                   </span>
                 </Button>
-                <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold sm:my-[50px] ">
                   Automate Your Work With Fuzzie
                 </h1>
               </div>
