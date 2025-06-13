bf398c5ae7fad665a40ff469496ccd97887dcf05
tajsviappeneure <tejasvi@appeneure>
2025-06-13 23:08:37 +0530

    Refactor layout component to improve structure and maintainability

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
       <Sidebar />
-      <div className="w-full">
+      <div className="w-full"> 
         {/* <InfoBar /> */}
         {props.children}
       </div>
